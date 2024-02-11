57558b9820295cc63a59f330bf839c5471ec79b4
DevTae <dev.taehyeon.kim@gmail.com>
2024-02-11 15:18:20 +0900

    Update docker file

--- nkedu-back/src/main/java/com/nkedu/back/repository/StudentRepository.java
+++ nkedu-back/src/main/java/com/nkedu/back/repository/StudentRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nkedu.back.entity.Student;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
